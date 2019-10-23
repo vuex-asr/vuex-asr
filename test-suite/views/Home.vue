948e20eafacf20b8f6772b242144b67723ca4859
Joris Wenting <jorislive@hotmail.com>
2019-10-23 16:41:16 +0200

    tests(): improve home text

--- test-suite/views/Home.vue
+++ test-suite/views/Home.vue
@@ -8,13 +8,13 @@
 
     <p class="help-text">
       For a more indepth guide on how to use and understand the possibilities of
-      the plugin, you could follow the
+      the plugin, please follow the
       <a href="url">learn by example</a> section of the
       <a href="url">manual</a>.
     </p>
 
     <p class="help-text">
-      To keep the the examples and tests lean and simple they are build with the
+      To keep the examples and tests lean and simple they are build with the
       analogy of having messages in the store and being able to mutate them. But
       of course you are able to bind any datatype (and method) available.
     </p>
